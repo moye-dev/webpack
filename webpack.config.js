ed826a5ed279414046bfe6df4ce7fe6c5ebffe6c
moye <2801845775@qq.com>
2021-03-16 17:28:45 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 var path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: 'index.[contenthash].js',
+  },
+   plugins: [new HtmlWebpackPlugin({
+       title:'xl',
+       template:'src/assets/index.html'
+   })],
+     module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
